0e74eda36e0e9064f1a6eb1775c800aad434e8e3
Manish Chaurasiya <Manish@Tests-MacBook-Pro.local>
2018-07-10 22:08:13 +0530

    Set up completed

---
+++ Amazon/Set 77/min_diff.cpp
@@ -0,0 +1,9 @@
+// Given an array of unsorted elements, find the minimum difference between any 2 elements in the array.
+#inlcude <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Basic Set up done!!";
+    return 0;
+}
