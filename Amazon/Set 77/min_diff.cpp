c5ae4af0783415ea476aa1dc9ff21cd5b2f18139
Manish Chaurasiya <Manish@tests-macbook-pro.local.symc.symantec.com>
2018-07-11 21:44:16 +0530

    minimum difference

--- Amazon/Set 77/min_diff.cpp
+++ Amazon/Set 77/min_diff.cpp
@@ -1,9 +1,26 @@
 // Given an array of unsorted elements, find the minimum difference between any 2 elements in the array.
-#inlcude <iostream>
+#include <iostream>
+#include <vector>
+#include <climits>
 
 using namespace std;
 
 int main() {
-    cout << "Basic Set up done!!";
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        vector<int> A(N);
+        for (int i = 0; i < N; i++) {
+            cin >> A[i];
+        }
+        sort(A.begin(), A.end());
+        int ans = INT_MAX;
+        for (int i = 1; i < N; i++) {
+            ans = min(ans, (A[i]-A[i-1]));
+        }
+        cout << ans << endl;
+    }
     return 0;
 }
